ea2f1a4e20cbb04861207cdd45c6fddaf5d8b394
nneveu <nneveu@hawk.iit.edu>
2017-09-17 23:28:44 -0500

    ipac/psi 2017 plot updates

--- ipac2017_measurements/ipac2017_measurements_rms_plots.py
+++ ipac2017_measurements/ipac2017_measurements_rms_plots.py
@@ -7,62 +7,84 @@ import matplotlib.backends.backend_pdf
 plt.rc('text', usetex=True)
 plt.rc('font',family='serif')
 
-before = glob.glob('/home/nicole/Documents/thesis_code/ipac2017_measurements/*.stat')
-#after  = glob.glob('/home/nicole/Documents/thesis_code/data/ipac2017/*weight*.stat')
-#fn = '/home/nicole/Documents/thesis_code/data/optLinac_weight0.stat'
-plotfn = './beamsizes_weight0.pdf'
-'''
-plt.axvline(x=3.1)
-plt.axvline(x=6.377)
-plt.axvline(x=8.76)
-plt.axvline(x=11.477)
-plt.axvline(x=14.848)
-'''
-pdf = matplotlib.backends.backend_pdf.PdfPages(plotfn) 
+runs =[0,1,5,7,9,10] 
 
-fig1, ax1 = plt.subplots()
-plt.title("Beam Sizes")
-plt.xlabel("z [m]")
 
-fig2, ax2 = plt.subplots()
-plt.title("Emittance")
-plt.xlabel("z [m]")
-plt.ylabel("xemit [um]")
-plt.axis([0.0,20.0,0.0,600])
+for w in runs:
+    #before = glob.glob('/home/nicole/Documents/thesis_code/ipac2017_measurements/*w1_*.stat')
+    before = glob.glob('/home/nicole/Documents/thesis_code/ipac2017_measurements/*_w'+str(w)+'_*.stat')
+    #after  = glob.glob('/home/nicole/Documents/thesis_code/data/ipac2017/*weight*.stat')
+    #fn = '/home/nicole/Documents/thesis_code/data/optLinac_weight0.stat'
+    plotfn = './beamsizes_weight'+str(w)+'.pdf'
+    '''
+    plt.axvline(x=3.1)
+    plt.axvline(x=6.377)
+    plt.axvline(x=8.76)
+    plt.axvline(x=11.477)
+    plt.axvline(x=14.848)
+    '''
+    pdf = matplotlib.backends.backend_pdf.PdfPages(plotfn) 
 
-fig3, ax3 = plt.subplots()
-plt.title('Bunch Length')
-plt.xlabel('z [m]')
-plt.ylabel('zrms [mm]')
+    fig1, ax1 = plt.subplots()
+    plt.title("Beam Sizes")
+    plt.xlabel("z [m]")
+    plt.grid('on')
+    
 
-for fn in before: 
-    print(fn)
-    legend = (fn.split('_')[3]).split('.')[0]
-    #print legend
-    parser = SddsReader(fn)                                                                                  
-    #x = []
-    z = parser.getColumn("s")
-    xrms = parser.getColumn("rms_x")
-    yrms = parser.getColumn("rms_y")
-    zrms = parser.getColumn("rms_s")
-    xemit = parser.getColumn("emit_x")
-                      
-    rmsymm = [l*10**3 for l in yrms]
-    rmsxmm = [m*10**3 for m in xrms]
-    rmszmm = [n*10**3 for n in zrms]
-    xemitum = [o*10**6 for o in xemit]
+    fig2, ax2 = plt.subplots()
+    plt.title("Emittance")
+    plt.xlabel("z [m]")
+    plt.ylabel("xemit [um]")
+    if int(w) < 7:
+        plt.axis([0.0,20.0,0.0,450])
+    else:
+        plt.axis([0.0,20.0,0.0,100])
+    plt.grid('on')
 
-    ax1.plot(z, rmsxmm, label=legend)
-    ax2.plot(z, xemitum, label=legend)
-    ax3.plot(z, rmszmm, label=legend)
+    fig3, ax3 = plt.subplots()
+    plt.title('Bunch Length')
+    plt.xlabel('z [m]')
+    plt.ylabel('zrms [mm]')
+    plt.grid('on')
 
-ax1.legend()
-ax2.legend()
-ax3.legend()
-pdf.savefig(fig1, dpi=1000, bbox_inches='tight')
-pdf.savefig(fig2, dpi=1000, bbox_inches='tight')
-pdf.savefig(fig3, dpi=1000, bbox_inches='tight')
-pdf.close()
+    fig4, ax4 = plt.subplots()
+    plt.title('Energy')
+    plt.xlabel('z [m]')
+    plt.ylabel('E [MeV]')
+    plt.grid('on')
+
+    for fn in before: 
+        print(fn)
+        legend = (fn.split('_')[4]).split('.')[0]
+        #print legend
+        parser = SddsReader(fn)                                                                                  
+        #x = []
+        z = parser.getColumn("s")
+        xrms = parser.getColumn("rms_x")
+        yrms = parser.getColumn("rms_y")
+        zrms = parser.getColumn("rms_s")
+        xemit = parser.getColumn("emit_x")
+        energy = parser.getColumn("energy")
+                  
+        rmsymm = [l*10**3 for l in yrms]
+        rmsxmm = [m*10**3 for m in xrms]
+        rmszmm = [n*10**3 for n in zrms]
+        xemitum = [o*10**6 for o in xemit]
+
+        ax1.plot(z, rmsxmm, label=legend)
+        ax2.plot(z, xemitum, label=legend)
+        ax3.plot(z, rmszmm, label=legend)
+        ax4.plot(z, energy, label=legend)
+
+    ax1.legend()
+    ax2.legend()
+    ax3.legend()
+    ax4.legend()
+    pdf.savefig(fig1, dpi=1000, bbox_inches='tight')
+    pdf.savefig(fig2, dpi=1000, bbox_inches='tight')
+    pdf.savefig(fig3, dpi=1000, bbox_inches='tight')
+    pdf.savefig(fig4, dpi=1000, bbox_inches='tight')
+    pdf.close()
 
 '''
 for fn2 in after:
--- ipac2017_measurements/ipac2017_measurements_yags.py
+++ ipac2017_measurements/ipac2017_measurements_yags.py
@@ -1,22 +1,38 @@
 from utils import H5Reader
+import h5py
 import glob 
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
+
+
+plotfn = 'weight7_YAGS.pdf'
+pdf = matplotlib.backends.backend_pdf.PdfPages(plotfn) 
 
 plt.rc('text', usetex=True)
 plt.rc('font',family='serif')
 
-yags  = glob.glob('/home/nicole/Documents/thesis_code/data/ipac2017/grad2/*w0*.stat')
+yags  = glob.glob('/home/nicole/Documents/thesis_code/ipac2017_measurements/*w7.h5')
+yags.sort(key=lambda f: int(filter(str.isdigit, f)))
+yags.append(yags.pop(1)) #putting CTR1 at back of file
+
 #fn = '/home/nicole/Documents/thesis_code/data/optLinac_weight0.stat'
-plotfn = 'weight0_YAGS.pdf'
-
- 
+point = 'Optimized Mostly Emittance, w7:\n' 
 for fn in yags: 
-    legend = (fn.split('_')[2]).split('.')[0]
+    fig = plt.figure()
+    legend = (fn.split('_')[2]).split('/')[1]
     #print legend
-    parser = H5Reader(fn)                                                                 
+    #parser = H5Reader(fn)                                                                 
     #plt.plot(x,ymm, label=legend)
+    plt.grid('on')
+    hf = h5py.File(fn, 'r') 
+    x  = np.asarray(hf.get('/Step#0'+'/x' ))*10.0**3.0 
+    y  = np.asarray(hf.get('/Step#0'+'/y'))*10.0**3.0  
+    plt.hist2d(x, y, bins = 200, range=[[-30.0,30.0], [-30.0,30.0]], cmin=1) 
+    plt.colorbar()
+    plt.xlabel("X [mm]")
+    plt.ylabel("Y [mm]")
+    plt.title(point+legend)
+    pdf.savefig( fig, bbox_inches='tight', dpi=900 )
 
-plt.xlabel("X [mm]")                                                                                                               
-plt.ylabel("Y [mm]")                                                                                 
-plt.title("Beam Sizes")
-plt.savefig(plotfn, bbox_inches='tight', dpi=900)
+pdf.close()
