fb5a5ce471bb5b33798dc5d590e98ff3eac8615b
nneveu <nneveu@hawk.iit.edu>
2017-09-24 01:37:12 -0500

    more functions added

--- data_analysis/psi_data.py
+++ data_analysis/psi_data.py
@@ -1,4 +1,4 @@
-import imageReader as ir
+from imageReader import readimage, difilter, view_each_frame, average_images, background_subtraction, similarity_check
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,17 +9,33 @@ import matplotlib.pyplot as plt
 testfile2 = '/home/nicole/Documents/thesis_code/data_analysis/gun_L1-L6_YAG6_FWHM1pt5_M250_R8pt5_GPhase-20_09-20-2017.dat'
 
 yag1 = './gun_L1-L6_YAG1_FWHM1pt5_M185_R-_GPhase-20_09-22-2017.dat'
+yag1_background = './gun_L1-L6_YAG1_FWHM1pt5_M185_R-_GPhase-20_background_09-22-2017.dat' 
+
 grasshopper = 'gun_L1-L6_YAG7_FWHM1pt5_M185_R-_GPhase-20_slit_29800_09-22-2017_Grasshopper3 GS3-PGE-23S6M_16376365_img.dat' 
+
+#Load background images
+#Deinterlace images with median filter
+#Average background into one image
+(bx, by,b_Nframes, background_array) = readimage(yag1_background)
+#Note, doing filter first removes more noise
+di_background  = difilter(background_array)
+#ave_background = average_images(background_array)#di_background)
+ave_background = average_images(di_background)
 
 
 #Load images
-(dx, dy, Nframes, image_array) = ir.readimage(yag1)
-print "Dx,Dy,NFrames= ",dx,dy,Nframes
+#Apply median filter to all frames
+#Subtract background
+(dx, dy, Nframes, image_array) = readimage(yag1)
+#print "Dx,Dy,NFrames= ",dx,dy,Nframes
+s = similarity_check(image_array)
+print s
+di_images = difilter(image_array)
+no_background_images = background_subtraction(di_images)
+#ave_image = average_images(no_background_images) 
 
 #View images
 #ir.view_each_frame(image_array)
-
-ir.average_images(image_array, dx, dy)
 
 
 
