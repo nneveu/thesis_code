adbfffd79777100669950d58b992bce6cbb77c84
nneveu <nneveu@hawk.iit.edu>
2017-10-01 16:39:35 -0500

    finished sigma calcs

--- data_analysis/psi_data.py
+++ data_analysis/psi_data.py
@@ -44,49 +44,82 @@ for f in fiducial_files:
 """
 #The following code  is used to do charge cut off
 # and analyize images
-ict_file_sdds = './charge/gun_L1-L6_YAG1_FWHM1pt5_M185_R-_GPhase-20_09-22-2017LC584AL.sdds'
-yag_background = './images/gun_L1-L6_YAG1_FWHM1pt5_M185_R-_GPhase-20_background_09-22-2017.dat'
-yag = './images/gun_L1-L6_YAG1_FWHM1pt5_M185_R-_GPhase-20_09-22-2017.dat'
- 
-#SDDS
-volts_array, cal = sdds_to_volts_array(ict_file_sdds)
-charge_array, scaled_volts = ict_charge(volts_array, data_type='sdds',cal_array=cal)
-#plot_ict_curves(scaled_volts, cal)
+ict_file_sdds  = glob('./charge/gun_L1-L6_YAG*.sdds')
+yag_backgrounds = glob('./images/gun_L1-L6_YAG*_FWHM1pt5_M185_R-_GPhase-20_*background*.dat')
+yags = glob('./images/gun_L1-L6_YAG*_FWHM1pt5_M185_R-_GPhase-20_09-2*-2017*.dat')
 
-#Load background images
-(bx, by,b_Nframes, background_array) = readimage(yag_background)
-#Deinterlace images with median filter
-#Note, doing filter first removes more noise
-di_background  = difilter(background_array)
-#Average background into one image 
-ave_background = average_images(di_background)
+#print len(ict_file_sdds)#, ict_file_sdds
+#print len(yag_background)#, yag_background
+#print len(yag)
 
-#Load images
-(dx, dy, Nframes, image_array) = readimage(yag)
-#print "Dx,Dy,NFrames= ",dx,dy,Nframes
-#Select only images with certain charge
-charge_images, n_images = select_on_charge(image_array, charge_array, 31.0, 29.0)
-#print np.shape(charge_images)
-#Apply median filter to all frames
-di_images = difilter(charge_images)
-#Subtract background
-no_background_images = background_subtraction(di_images, ave_background)
-#view_each_frame(no_background_images)
-#ave_no_back = average_images(no_background_images)
+for ict_file in ict_file_sdds: 
+   if 'YAG1' in ict_file:
+       key  = 'yag1'
+       find = 'YAG1'
+       yag_back =  [s for s in yag_backgrounds if find in s]
+       yag      =  [s for s in yags if find in s] 
+   elif 'YAG2' in ict_file:
+       key  = 'yag2'
+       find = 'YAG2'
+       yag_back =  [s for s in yag_backgrounds if find in s] 
+       yag      =  [s for s in yags if find in s] 
+   elif 'YAG3' in ict_file:
+       key = 'yag3'
+       find = 'YAG3'
+       yag_back =  [s for s in yag_backgrounds if find in s] 
+       yag      =  [s for s in yags if find in s] 
+   elif 'YAG6' in ict_file:
+       key = 'yag6'
+       find = 'YAG6'
+       yag_back =  [s for s in yag_backgrounds if find in s] 
+       yag      =  [s for s in yags if find in s] 
+   elif 'YAG7' in ict_file:
+       key = 'yag7'
+       find = 'YAG7'
+       yag_back =  [s for s in yag_backgrounds if find in s]
+       yag      =  [s for s in yags if find in s]  
+   elif 'CTR' in ict_file:
+       key = 'yagCTR'
+       find = 'CTR'
+       yag_back =  [s for s in yag_backgrounds if find in s] 
+       yag      =  [s for s in yags if find in s]  
+   else: 
+       print 'bad key'
 
-#Starting to find fits
-fiducials    = np.load('psi_fiducials.npy').flatten()
-sigmax   = np.zeros((n_images))
-sigmay   = np.zeros((n_images))
+   print yag_back, yag
 
-for n in range(0,n_images):
-    raw_x, raw_y = raw_data_curves(no_background_images[:,:,n], dx, dy)
-    x_axis, sigmax[n] = fit_data(raw_y, fiducials[0]['yag1'])
-    x_axis, sigmay[n] = fit_data(raw_y, fiducials[0]['yag1'])
-print sigmax, sigmay
+   #SDDS
+   volts_array, cal = sdds_to_volts_array(ict_file)
+   charge_array, scaled_volts = ict_charge(volts_array, data_type='sdds',cal_array=cal)
+   #plot_ict_curves(scaled_volts, cal)
 
-#print x_axis
+   #Load background images
+   (bx, by,b_Nframes, background_array) = readimage(yag_back[0])
+   #Deinterlace images with median filter
+   #Note, doing filter first removes more noise
+   di_background  = difilter(background_array)
+   #Average background into one image 
+   ave_background = average_images(di_background)
 
+   #Load images
+   (dx, dy, Nframes, image_array) = readimage(yag[0])
+   #print "Dx,Dy,NFrames= ",dx,dy,Nframes
+   #Select only images with certain charge
+   charge_images, n_images = select_on_charge(image_array, charge_array, 40.0, 33.0)
+   #print np.shape(charge_images)
+   #Apply median filter to all frames
+   di_images = difilter(charge_images)
+   #Subtract background
+   no_background_images = background_subtraction(di_images, ave_background)
+   #view_each_frame(no_background_images)
+   #ave_no_back = average_images(no_background_images)
+
+   #Starting to find fits
+   fiducials = np.load('psi_fiducials.npy').flatten()
+   #This gives x and y beam sizes 
+   beamsizes = fit_data(no_background_images, fiducials, key)
+
+'''
 #plt.figure(100)
 #plt.plot(x_axis, raw_x)
 #plt.show()
@@ -106,8 +139,4 @@ print sigmax, sigmay
 #volts_array, time_array = csv_to_volts_array(ict_file_csv)
 #charge_array, volts_scaled = ict_charge(volts_array, time_array=time_array, data_type='csv')
 #plot_ict_curves(volts_scaled, time_array=time_array)
-
-
-
-
 
