376db0cd5bd40883cc50ea192f56db3742b324a4
nneveu <nneveu@hawk.iit.edu>
2017-10-16 21:12:03 -0500

    Made regression test more general

--- regression_test.py
+++ regression_test.py
@@ -17,12 +17,16 @@ import sys
 sys.path.append('/Users/nneveu/Documents/PythonScripts')
 import myplots as mplt
 
+n = 4
 
 dataopal1 = mplt.load('1nCR0pt75LaserAstraM273.stat')
-dataopal2 = mplt.load('RFphotoinjector.stat')
+dataopal2 = mplt.load('RFphotoinjector_John.stat') #-1.9.stat')
+dataopal3 = mplt.load('RFphotoinjector-1.6_binary.stat')
+dataopal4 = mplt.load('RFphotoinjector-1.9.stat') #-1.9.stat')
 
-v1 = str(1.4)
-v2 = str(1.9)
+data = [dataopal1, dataopal2, dataopal3, dataopal4]
+
+v = [str(1.4), str(1.6), str(1.6) + ' binary', str(1.9)]
 
 # Four axes, returned as a 2-d array
 f, axarr = plt.subplots(2, 2, figsize=(7.5,6))
@@ -73,44 +77,57 @@ f.subplots_adjust(wspace=.4)
 # 
 # plt.savefig('paperfigAstrainGungrid46.png', format='png', dpi=1000, bbox_inches='tight')
 #==============================================================================
-
-axarr[0, 0].plot(dataopal1['z'], dataopal1['xrms'], 'g-', label = 'OPAL V'+v1)
-axarr[0, 0].plot(dataopal2['z'], dataopal2['xrms'], '--', label = 'OPAL V'+v2)
+#axarr[0, 0].plot(dataopal1['z'], dataopal1['xrms'], 'g-', label = 'OPAL V'+v1)
+#axarr[0, 0].plot(dataopal2['z'], dataopal2['xrms'], '--', label = 'OPAL V'+v2)
 axarr[0, 0].set_title('Transverse Beam Size',fontsize=18)
 axarr[0, 0].set_xlabel('z [m]',fontsize=18)
 axarr[0, 0].set_ylabel(r'$x_{rms}$ [mm]',fontsize=20)
 axarr[0, 0].set_xticks(np.arange(0.0, 5.0, 1.0) )
 axarr[0, 0].axis([0.0,4.0,0,3.0])
-axarr[0, 0].legend(loc='best')
+#axarr[0, 0].legend(loc='best')
 
-axarr[1, 0].plot(dataopal1['z'], dataopal1['zrms'], 'g-',  label = 'OPAL V'+v1)
-axarr[1, 0].plot(dataopal2['z'], dataopal2['zrms'], '--',  label = 'OPAL V'+v2)
+#axarr[1, 0].plot(dataopal1['z'], dataopal1['zrms'], 'g-',  label = 'OPAL V'+v1)
+#axarr[1, 0].plot(dataopal2['z'], dataopal2['zrms'], '--',  label = 'OPAL V'+v2)
 axarr[1, 0].set_title('Bunch Length',fontsize=18)
 axarr[1, 0].set_xlabel('z [m]',fontsize=18)
 axarr[1, 0].set_ylabel('$z_{rms}$ [mm]',fontsize=20)
 axarr[1, 0].set_xticks(np.arange(0.0, 5.0, 1.0) )
 axarr[1, 0].axis([0.0,4.0,0,2.5])
-axarr[1, 0].legend(loc='best')
+#axarr[1, 0].legend(loc='best')
 
-axarr[0, 1].plot(dataopal1['z'], dataopal1['xemit'], 'g-', label = 'OPAL V'+v1)
-axarr[0, 1].plot(dataopal2['z'], dataopal2['xemit'], '--', label = 'OPAL V'+v2)
+#axarr[0, 1].plot(dataopal1['z'], dataopal1['xemit'], 'g-', label = 'OPAL V'+v1)
+#axarr[0, 1].plot(dataopal2['z'], dataopal2['xemit'], '--', label = 'OPAL V'+v2)
 axarr[0, 1].set_title('Normalized Emittance',fontsize=18)
 axarr[0, 1].set_xlabel('z [m]',fontsize=18)
 axarr[0, 1].set_ylabel('$\epsilon_{nx}$ [$\mu$m]',fontsize=20)
 axarr[0, 1].set_xticks(np.arange(0.0, 5.0, 1.0) )
 axarr[0, 1].set_yticks(np.arange(0.0, 14, 2.0) )
 axarr[0, 1].axis([0.0,4.0,0,14])
-axarr[0, 1].legend(loc='upper right')
+#axarr[0, 1].legend(loc='upper right')
 
 
-axarr[1, 1].plot(dataopal1['z'], dataopal1['E']+0.511, 'g-', label = 'OPAL V'+v1)
-axarr[1, 1].plot(dataopal2['z'], dataopal2['E']+0.511, '--', label = 'OPAL V'+v2)
+#axarr[1, 1].plot(dataopal1['z'], dataopal1['E']+0.511, 'g-', label = 'OPAL V'+v1)
+#axarr[1, 1].plot(dataopal2['z'], dataopal2['E']+0.511, '--', label = 'OPAL V'+v2)
 axarr[1, 1].set_title('Energy ',fontsize=18)
 axarr[1, 1].set_xlabel('z [m]',fontsize=18)
 axarr[1, 1].set_ylabel('$\gamma mc^2$ [MeV]',fontsize=20)
 axarr[1, 1].set_xticks(np.arange(0.0, 5.0, 1.0) )
 axarr[1, 1].axis([0.0,4.0,0,8.0])
+#axarr[1, 1].legend(loc='lower right')
+
+markers = ['g-', 'b--', 'c-', 'r--']
+for i in range(0,len(v)):
+    data_n = data[i]
+    axarr[0, 0].plot(data_n['z'], data_n['xrms'], markers[i], label = 'OPAL V'+v[i])
+    axarr[1, 0].plot(data_n['z'], data_n['zrms'], markers[i], label = 'OPAL V'+v[i])
+    axarr[0, 1].plot(data_n['z'], data_n['xemit'], markers[i], label = 'OPAL V'+v[i])   
+    axarr[1, 1].plot(data_n['z'], data_n['E']+0.511, markers[i], label = 'OPAL V'+v[i]) 
+
+axarr[0, 0].legend(loc='best')
+axarr[1, 0].legend(loc='best')
+axarr[0, 1].legend(loc='upper right')
 axarr[1, 1].legend(loc='lower right')
+
 
 plt.savefig('./regtest.pdf', format='pdf', dpi=1000, bbox_inches='tight')
 
@@ -118,6 +135,7 @@ plt.savefig('./regtest.pdf', format='pdf', dpi=1000, bbox_inches='tight')
 #plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
 #plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
 
+'''
 plt.figure()
 plt.plot(dataopal1['z'], dataopal1['Bz'], label='OPAL V'+v1)
 plt.plot(dataopal2['z'], dataopal2['Bz'], '*', markevery=200, label='OPAL V'+v2)
@@ -127,5 +145,6 @@ plt.figure()
 plt.plot(dataopal1['z'], dataopal1['Ez'], label='OPAL V'+v1)
 plt.plot(dataopal2['z'], dataopal2['Ez'], '*', markevery=200, label='OPAL V'+v2)
 plt.legend()
+'''
 
 plt.show()
