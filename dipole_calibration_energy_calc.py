80c806806c99e8f9448fe45610f1a6bdeda3e801
nicole <nneveu@hawk.iit.edu>
2018-07-05 11:28:03 -0500

    more energy measurments

--- dipole_calibration_energy_calc.py
+++ dipole_calibration_energy_calc.py
@@ -20,7 +20,6 @@ def counts_to_current(counts):
     current = poly[0]*counts + poly[1]
     return current
 
-
 def current_to_bfield(current):
     #from gao's script to pull info from plot
     #B in Tesla
@@ -31,21 +30,30 @@ def counts_to_energy(rho, counts):
     i = counts_to_current(counts)    
     #B in Tesla
     bfield = current_to_bfield(i)
-    energy = ((bfield*rho)/3.335641)*10**3
-    return energy    
-
-def total_energy(momentum):
+    #Energy in MeV
+    momentum = ((bfield*rho)/3.335641)*10**3
     total_e = np.sqrt(0.511**2+momentum**2)
     print( 'momentum', momentum)
-    print( 'total energy', total_e)
+    print( 'total energy', total_e, '\n')
     return total_e
 
-angle = 20 #19 #degrees
+def current_to_energy(rho, current):
+    bfield = current_to_bfield(current)
+    #Energy in MeV
+    momentum = ((bfield*rho)/3.335641)*10**3
+    
+    total_e = np.sqrt(0.511**2+momentum**2)
+    print( 'momentum', momentum)
+    print( 'total energy', total_e, '\n')
+    return total_e
+
+angle = 20 #degrees
 leff = 0.3154
 
 rho = calc_rho(leff, angle)
-print("rho", rho) 
+print("rho", rho, '\n') 
 
+print('Measurements Pre-2018')
 mean1       = np.mean([5826,5763]) #,5632 mediean 11/02
 mean2       = np.mean([10054,9921,9968,9926])
 #mean3      = np.mean([14688]) #bad measurement?
@@ -58,18 +66,17 @@ gun_l1l2_l3l5 = counts_to_energy(rho, mean2)#current_to_bfield(counts_to_current
 gun_l1_to_l6  = counts_to_energy(rho, mean3)#current_to_bfield(counts_to_current(mean3))
 high_q_energy = counts_to_energy(rho, mean3_highq)
 
-
-e1 = total_energy(gun)
-e2 = total_energy(gun_l1l2)
-e3 = total_energy(gun_l1l2_l3l5)
-e4 = total_energy(gun_l1_to_l6)
-e5 = total_energy(high_q_energy)
+#e1 = total_energy(gun)
+#e2 = total_energy(gun_l1l2)
+#e3 = total_energy(gun_l1l2_l3l5)
+#e4 = total_energy(gun_l1_to_l6)
+#e5 = total_energy(high_q_energy)
 
 #energy spread on 1nc 11-02
-lowenergy  = counts_to_energy(rho, 5376)
-highenergy = counts_to_energy(rho, 5888)
-l = total_energy(lowenergy)
-h = total_energy(highenergy)
+#lowenergy  = counts_to_energy(rho, 5376)
+#highenergy = counts_to_energy(rho, 5888)
+#l = total_energy(lowenergy)
+#h = total_energy(highenergy)
 #momentum = ((gun*rho)/3.3356)*10**3
 #total_e = total_energy(momentum)
 
@@ -78,7 +85,26 @@ h = total_energy(highenergy)
 #plt.plot(counts, current)
 #plt.show()
 
+print('Measurements 2018 \n')
+#current_to_energy(rho, 0.8)
+print('gun, bad measurement')
+counts_to_energy(rho, 1408)
+
+print('Gun, L4, L6')
+counts_to_energy(rho, 4770)
+
+print('Gun, L4, L6, bad measurement - uncentered')
+counts_to_energy(rho, 4736)
+
+print('Gun, L3, L5, bad measurment - uncentered')
+counts_to_energy(rho, 5184)
 
 
+print('Gun, L1, L2, bad measurement - uncentered')
+counts_to_energy(rho, 5408)
 
-
+print('Gun L1-L6, full energy')
+print('max energy:')
+counts_to_energy(rho, 13503)
+print('min energy:')
+counts_to_energy(rho, 12768)
